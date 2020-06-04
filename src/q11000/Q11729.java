81d8a7c387b5f5e2087a6e4c8e32a145b0ef36a2
YEONJA2 <33532366+mangekyou93@users.noreply.github.com>
2020-06-04 15:01:40 +0900

    재귀
    재귀 끝
    
    별찍기랑 하노이는 너무 어려움...
    꼭 다시 한 번 풀어보기 바람

--- src/q11000/Q11729.java
+++ src/q11000/Q11729.java
@@ -6,11 +6,43 @@ import java.io.InputStreamReader;
 public class Q11729 {
 
 	//백준 문제 11729 : 하노이 탑 이동 순서
-	public static void main(String[] args) {
+	//하노이 문제는 대표적인 분할정복(divide-and-conquer) 문제
+	//메모리 98MB / 시간 0.34 s
+	//재귀함수라서 시간은 많이 소모됨.
+	static StringBuilder sb = new StringBuilder();
+	
+	
+	public static void main(String[] args) throws Exception{
 
+		//1 -> 1
+		//2 -> 3
+		//3 -> 7
+		//4 -> 15
+		//5 -> 31
+		//2^n  - 1
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		int n = Integer.parseInt(br.readLine());
 		
+		int k = (int)(Math.pow(2, n)) - 1;
+		sb.append(k+"\n");
+		
+		//n은 탑의 층 수 / 1,2,3은 원판의 위치.
+		//1 -> 3으로 이동하는 것을 뜻한다.
+		hanoi(n,1,2,3);
+		
+		System.out.println(sb);
+	}
+	
+	public static void hanoi(int n, int from, int by, int to) {
+		if(n == 1) {
+			sb.append(from+" "+to+"\n");
+			return ;
+		}
+		hanoi(n-1,from,to,by);
+		sb.append(from+" "+to+"\n");
+		hanoi(n-1,by,from,to);
 	}
 
 }
--- src/q2000/Q2447.java
+++ src/q2000/Q2447.java
@@ -6,18 +6,89 @@ import java.io.InputStreamReader;
 public class Q2447 {
 
 	//백준 문제 2447 : 별 찍기 - 10
+	//이것도 역시 분할정복 : 앞에서 피보나치도 작은단위로 쪼개면서 결국 마지막에 피보나치수열이 완성됨.
+	//가장 작을 단위까지 재귀를 호출하면서 가장 작을 단위가 될 때, 하나씩 배열을 채워감.
+	
+	//N=3
+	//***
+	//* *
+	//***
+	
+	//N=9=3^2
+	//*********
+	//* ** ** *
+	//*********
+	//***   ***
+	//* *   * *
+	//***   ***
+	//*********
+	//* ** ** *
+	//*********
+	
+	//2차원 배열, N=3이면 star[1][1]이 공백
+	//즉, [0][0] -> [0][1] -> [0][2] -> [1][0] : 별을 채우고 [1][1]은 공백으로
+	//N=9이면 N=3인 것을 똑같이 채우고 5번째일 떄 공백
+	
+	static char[][] star;
+	
 	public static void main(String[] args) throws Exception {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
+		star = new char[n][n];
 		
-		printStar(n);
+		printStar(n, 0, 0, false);
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				sb.append(star[i][j]);
+			}
+			sb.append('\n');
+		}
+		System.out.println(sb);
+		
 	}
 	
 	
-	public static void printStar(int n) {
+	public static void printStar(int n, int x, int y, boolean blank) {
 		
+		//공백 칸일 경우
+		if(blank) {
+			for(int i=x ; i<x+n ; i++) {
+				for(int j=y ; j<y+n ; j++) {
+					star[i][j] = ' ';
+				}
+			}
+			return ;
+		}
 		
+		if(n == 1) {
+			star[x][y] = '*';
+			return ;
+		}
+		
+		/*
+		N=27 일 경우 한 블록의 사이즈는 9이고, 
+		N=9 일 경우 한 블록의 사이즈는 3이듯
+		해당 블록의 한 칸을 담을 변수를 의미 size
+        
+		count는 별 출력 누적을 의미
+		*/
+
+		int size = n / 3;
+		int count = 0;
+		for (int i = x; i < x + n; i += size) {
+			for (int j = y; j < y + n; j += size) {
+				count++;
+				if (count == 5) { // 공백 칸일 경우
+					printStar(size, i, j, true);
+				} else {
+					printStar(size, i, j, false);
+				}
+			}
+		}
 	}
 }
